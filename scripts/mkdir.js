f4daa3923d618a9bc85f534f46a61589782e3a25
karashiiro <49822414+karashiiro@users.noreply.github.com>
2023-02-07 19:53:06 -0800

    Fix deploy script

--- scripts/mkdir.js
+++ scripts/mkdir.js
@@ -1,6 +1,6 @@
 import fs from 'node:fs';
 
-fs.writeFile('./out/.nojekyll', '', (err) => {
+fs.mkdir('./out', { recursive: true }, (err) => {
     if (err != null) {
         console.error(err);
         process.exit(1);
